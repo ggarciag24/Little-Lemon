a153a87ae884c5ba1e52c8422ccebc8fb31f08db
German Garcia Gonzalez <ggarciag.business@outlook.com>
2023-03-22 12:09:35 -0400

    Nav component styled using flexbox for desktop view

--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,9 @@ import Logo from "./images/logo.png";
 
 function Nav() {
     return(
-        <nav>
-            <img src={Logo}/>
-            <ul>
+        <nav >
+            <ul className="nav-list">
+                <li><img className="main-logo" src={Logo}/></li>
                 <li><a href="/home">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/menu">Menu</a></li>
