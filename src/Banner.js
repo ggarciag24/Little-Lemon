eaae1fe1a31db5f58c24f804736ce4e0a436bd39
German Garcia Gonzalez <ggarciag.business@outlook.com>
2023-03-23 14:16:19 -0400

    reserve a table button on main page directs user to reservations page

--- src/Banner.js
+++ src/Banner.js
@@ -1,4 +1,5 @@
 import bannerimg from "./images/bannerImage.jpg";
+import {Link} from "react-router-dom"
 
 function Banner() {
     return(
@@ -7,7 +8,7 @@ function Banner() {
                 <h1>Little Lemon</h1>
                 <h3>Chicago</h3>
                 <p>We are a family owned mediteranean restaurant that prides itself in great service and quality food</p>
-                <button>Reserve a table</button>
+                <Link to="/reservations" ><button className="banner-button">Reserve a table</button></Link>
             </div>
             <div>
                 <img className="banner-img" src={bannerimg}/>
