a77f86ac955b7c727cb87aa20885a2824c560396
German Garcia Gonzalez <ggarciag.business@outlook.com>
2023-03-27 12:18:58 -0400

    About page has been populated and styled

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,9 +1,22 @@
-
+import greekSalad from "./images/greek-salad.jpg";
+import spread from "./images/bannerImage.jpg";
+import bruchetta from "./images/bruchetta.png";
+import lemonDessert from "./images/lemon-dessert.jpg";
 
 function AboutPage() {
     return(
         <div>
-            About Page
+            <div className="about-banner">
+            <h1>About</h1>
+            <h1>Little Lemon</h1>
+            <p>Little Lemon is a family owned Mediteranean restaurant that takes pride in providing great customer service and quality foods. We hope to create a memorable experience that will make you want to come back and enjoy yourselfs time and time again. We have a variety of foods and are our dedicated chefs are very willing to edit dishes to your preference. You can contact us through phone or email, our phone number is: <a>(000) 000-0000</a> and our email is: <a>exampleEmail@example.com</a></p>
+            </div>
+            <div className="banner-images">
+                <img src={bruchetta}/>
+                <img src={lemonDessert}/>
+                <img src={greekSalad}/>
+                <img src={spread}/>
+            </div>
         </div>
     )
 }
