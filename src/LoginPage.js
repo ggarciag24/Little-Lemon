531b782ebd0669ca69ca620dd5446f3a8ff32273
German Garcia Gonzalez <ggarciag.business@outlook.com>
2023-03-27 15:11:54 -0400

    login page structure and styling complete (no functionality)

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -2,8 +2,24 @@
 
 function LoginPage() {
     return(
-        <div>
-            Login Page
+        <div className="login-container">
+            <h1>Login</h1>
+            <form className="login-form">
+                <div>
+                <label htmlFor="email">Email </label>
+                <input id="email"></input>
+                </div>
+                <div>
+                <label htmlFor="pw">Password </label>
+                <input id="pw"></input>
+                </div>
+                <div>
+                <p>Create an account <a style={{textDecorationLine: 'underline'}}>here</a></p>
+                </div>
+                <div>
+                <button className="login-btn">Login</button>
+                </div>
+            </form>
         </div>
     )
 }
