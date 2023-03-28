39836636e25116155d3ebb64c1721c02b2f203d4
German Garcia Gonzalez <ggarciag.business@outlook.com>
2023-03-28 13:19:14 -0400

    menu page has been populated and styled

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import lemonDessert from "./images/lemon-dessert.jpg";
 import FoodCard from "./FoodCard";
 
 function Main() {
-    const foods = [{img: greekSalad, name: "Greek Salad", price: 12.99, description: "The famous greek salad with crispy lettuce, peppers, olives, and our chicago style feta cheese, garnished with crunchy garlic and rosemary crutons."}, {img: bruchetta, name: "Bruchetta", price: 5.99, description: "Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."}, {img: lemonDessert, name: "Lemon Dessert", price: 5.00, description: "This come straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."}];
+    const foods = [{img: greekSalad, name: "Greek Salad", price: 12.99, description: "The famous greek salad with crispy lettuce, peppers, olives, and our chicago style feta cheese, garnished with crunchy garlic and rosemary crutons."}, {img: bruchetta, name: "Bruchetta", price: 5.99, description: "Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil."}, {img: lemonDessert, name: "Lemon Dessert", price: "5.00", description: "This come straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined."}];
 
     return(
         <main>
